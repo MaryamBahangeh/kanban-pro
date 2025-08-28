f19bc0a6664eea9fbe2d69b5ac503bf4666bc8aa
Maryam.bahangeh <maryam.bahangeh@gmail.com>
2025-08-28 14:23:16 +0200

    [ADD] routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+
 import { BrowserRouter } from "react-router";
 
+import { ErrorBoundary } from "react-error-boundary";
+
+import ErrorPage from "@/pages/ErrorPage/ErrorPage.tsx";
+
+import App from "./App.tsx";
+
+import "./index.css";
+import "./styles/colors.css";
+import "./styles/shadows.css";
+import "./styles/shapes.css";
+import "./styles/typography.css";
+
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ErrorBoundary FallbackComponent={ErrorPage}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    , //{" "}
+  </ErrorBoundary>,
 );
